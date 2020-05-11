1537418c579dded0ed18ba21dc4ee203ccf5ea63
dark0ghost <diamondlego018@gmail.com>
2020-05-11 22:35:48 +0200

    fix

--- a.py
+++ a.py
@@ -1,3 +1,4 @@
+from target.release import libfastpython
 
-
+print(libfastpython.sum_as_string(1,6))
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,25 @@ mod tests {
     fn it_works() {
 
     }
+
+    #[test]
+    fn test() {
+
+    }
 }
 
-#[macro_use]
+//#[macro_use]
 extern crate cpython;
 
-mod lid_extend;
-use lid_extend::extend_lib;
+use cpython::{PyResult, Python, py_module_initializer, py_fn};
 
-use cpython::{Python, PyResult};
+pub fn s(c:i64){
 
-fn count_doubles(_: Python,a: i64 , b: i64) -> PyResult<i64> {
-    let c: i64 = a*b;
-    Ok(c)
 }
 
-py_module_initializer!(rust2py, |py, m| {
+py_module_initializer!(libfastpython, |py, m| {
     m.add(py, "__doc__", "This module is implemented in Rust.")?;
-    m.add(py, "sum_as_string", py_fn!(py,count_doubles(a: i64 , b: i64)))?;
+    m.add(py, "sum_as_string", py_fn!(py, s(c)))?;
     Ok(())
 });
+
